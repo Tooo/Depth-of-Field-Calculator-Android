fa43902efa4bb1ec876240825cd73795bb317967
tsc7 <travis_chan@sfu.ca>
2020-06-05 13:56:31 -0700

    Change simple texts
    Removed test button, changed colours of header, and modify text in DOF Calculator screen

--- app/src/main/java/com/cmpt276a2/MainActivity.java
+++ app/src/main/java/com/cmpt276a2/MainActivity.java
@@ -9,22 +9,11 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private  static final String TAG = "TestApp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = (Button) findViewById(R.id.btn_test);
-
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(TAG, "Test test message");
-                Toast.makeText(getApplicationContext(), "It's magic", Toast.LENGTH_SHORT)
-                .show();
-            }
-        });
     }
 }
