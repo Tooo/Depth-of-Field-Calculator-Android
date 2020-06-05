8941d6fec509469d816da0059dd30442a072b385
tsc7 <travis_chan@sfu.ca>
2020-06-05 14:29:25 -0700

    Add Lens FAB
    Add Lens Activity, FAB button, changed titles

---
+++ app/src/main/java/com/cmpt276a2/AddLens.java
@@ -0,0 +1,14 @@
+package com.cmpt276a2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddLens extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_lens);
+    }
+}
--- app/src/main/java/com/cmpt276a2/MainActivity.java
+++ app/src/main/java/com/cmpt276a2/MainActivity.java
@@ -2,11 +2,14 @@ package com.cmpt276a2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupAddButton();
+    }
+
+    private void setupAddButton () {
+        FloatingActionButton fab = findViewById(R.id.fab_add);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AddLens.class);
+                startActivity(intent);
+            }
+        });
     }
 }
