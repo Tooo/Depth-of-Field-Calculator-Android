4061cf2b2ed53e2bf183ea5fa376a549e061748e
tsc7 <travis_chan@sfu.ca>
2020-06-05 17:51:43 -0700

    Test Commit
    Move folders, testing if works

--- app/src/main/java/com/cmpt276a2/MainActivity.java
+++ app/src/main/java/com/cmpt276a2/MainActivity.java
@@ -4,10 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
